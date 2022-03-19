efeebbc82e919eb777cddea000f2fd503971ed5b
JacobFeat <jakubk198@gmail.com>
2022-03-19 19:03:35 +0100

    add new component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 
+function giveSurnameFirst({firstName, surname}){
+  return `${surname} ${firstName}`;
+}
+
+const student = {
+  firstName: "John",
+  surname: "Jadovsky",
+  age: 24,
+};
+
+const studentIntroduction = (
+  <p>{giveSurnameFirst(student)}</p>
+);
+
+function Welcome({name, age}) {
+  return (
+    <div>
+      <h1>Hello, {name}</h1>
+      <p>You're {age}</p>
+    </div>
+  )
+}
+
+const person = <Welcome name={studentIntroduction} age={student.age} />;
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        {person}
+        {studentIntroduction}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
